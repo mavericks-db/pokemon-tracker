cfcae3529f8f99509ede0c67c6c4b7c2ea5c0ca6
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-11-26 22:25:28 +0500

    Check max stats limit before submitting pokemons to a league

--- client/src/components/LeagueCard.js
+++ client/src/components/LeagueCard.js
@@ -6,10 +6,18 @@ function LeagueCard() {
   const location = useLocation();
   const navigate = useNavigate();
   const {
-    id, title, leaguelocation, terrain, date, slots, maxstats, jsonPokemon,
+    id,
+    title,
+    leaguelocation,
+    terrain,
+    date,
+    slots,
+    maxstats,
+    jsonPokemon,
   } = location.state;
   const apiURL = 'http://localhost:5000/api/my_pokemons';
   const saveURL = 'http://localhost:5000/api/updateleague';
+  const statsURL = 'http://localhost:5000/api/selectpokemon';
   const availslots = new Array(slots).fill('');
   const [arr, setArr] = useState();
   let checkSelection = [];
@@ -64,6 +72,34 @@ function LeagueCard() {
   };
 
   const saveHandler = () => {
+    const spanAll = document.getElementsByClassName('spanStats');
+    const totalArr = [];
+    Array.from(spanAll).forEach((el) => {
+      totalArr.push(parseInt(el.innerText, 10));
+    });
+    const total = totalArr.reduce((a, b) => a + b);
+    const errorMsg = document.querySelector('#error_msg');
+    if (total > maxstats) {
+      errorMsg.textContent = 'The sum of the total stats of all slots should not exceed the total maximum stats allowed for the league. Please choose again. Reloading in 3 seconds.';
+      // const btnAll = document.querySelectorAll('.confirm');
+      // checkSelection = [];
+      // Array.from(spanAll).forEach((i) => {
+      //   const el = i;
+      //   console.log(i.textContent);
+      //   el.textContent = 0;
+      // });
+      // btnAll.forEach((button) => {
+      //   button.removeAttribute('disabled');
+      // });
+
+      setTimeout(() => {
+        errorMsg.textContent = ' ';
+        window.location.reload();
+      }, 3000);
+
+      return null;
+    }
+
     async function fetchData() {
       await fetch(saveURL, {
         method: 'POST',
@@ -75,18 +111,60 @@ function LeagueCard() {
     }
     fetchData();
     navigate('/all_leagues');
+    return null;
+  };
+
+  const statsHandler = (e) => {
+    const target1 = e.target.nextSibling.id;
+    const obj = {
+      pokemon: e.target.value,
+    };
+    async function fetchData() {
+      const response = await fetch(statsURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+      });
+      const data = await response.json();
+      const sibling = document.querySelector(`#${target1}`);
+      sibling.textContent = data[0].attack + data[0].defense + data[0].speed;
+    }
+    fetchData();
   };
 
   return (
     <>
       <h1>This is a league card</h1>
-      <h3>{id}</h3>
-      <h3>{title}</h3>
-      <h5>{leaguelocation}</h5>
-      <h5>{terrain}</h5>
-      <h5>{date}</h5>
-      <h5>{maxstats}</h5>
-      <h5>{slots}</h5>
+      <h3>
+        League ID:
+        {id}
+      </h3>
+      <h3>
+        Title:
+        {title}
+      </h3>
+      <h5>
+        Location:
+        {leaguelocation}
+      </h5>
+      <h5>
+        Terrain:
+        {terrain}
+      </h5>
+      <h5>
+        Date:
+        {date}
+      </h5>
+      <h5>
+        Total Maximum Stats:
+        {maxstats}
+      </h5>
+      <h5>
+        No. of required slots:
+        {slots}
+      </h5>
       <table>
         <thead>
           <tr>
@@ -104,8 +182,6 @@ function LeagueCard() {
                     {pokemon.map((slot, idx) => (
                       <h5 key={nanoid()}>
                         {`${idx + 1}.)`}
-                        {' '}
-&nbsp;
                         {' '}
                         {slot === 'solo' ? '---' : slot}
                       </h5>
@@ -140,7 +216,10 @@ function LeagueCard() {
                 <tr>
                   <td key={nanoid()}>{idx + 1}</td>
                   <td>
-                    <select id={`slotA_${idx + 1}`}>
+                    <select
+                      id={`slotA_${idx + 1}`}
+                      onChange={(e) => statsHandler(e)}
+                    >
                       <option value="0">&nbsp;</option>
                       {arr ? (
                         arr.map((pokemon) => (
@@ -152,9 +231,15 @@ function LeagueCard() {
                         <option value="0">&nbsp;</option>
                       )}
                     </select>
+                    <span id={`spanA_${idx + 1}`} className="spanStats">
+                      0
+                    </span>
                   </td>
                   <td>
-                    <select id={`slotB_${idx + 1}`}>
+                    <select
+                      id={`slotB_${idx + 1}`}
+                      onChange={(e) => statsHandler(e)}
+                    >
                       <option value="0">&nbsp;</option>
                       {arr ? (
                         arr.map((pokemon) => (
@@ -166,6 +251,9 @@ function LeagueCard() {
                         <option value="0">&nbsp;</option>
                       )}
                     </select>
+                    <span id={`spanB_${idx + 1}`} className="spanStats">
+                      0
+                    </span>
                   </td>
                   <td>
                     <button
